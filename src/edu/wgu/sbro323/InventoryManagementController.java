a8f69e65476cd341a3d52dbf4c8fdb5edc845b5c
elitebook <elitebook@EliteBook>
2017-04-19 16:51:21 +0200

    Create addpart GUI

--- src/edu/wgu/sbro323/InventoryManagementController.java
+++ src/edu/wgu/sbro323/InventoryManagementController.java
@@ -4,10 +4,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 /**
@@ -17,18 +19,36 @@ import javafx.stage.Stage;
 public class InventoryManagementController implements Initializable {
     
     @FXML
-    private Label label;
     private Button btnExit;
     
 
     
     @FXML
-    private void exitAction(ActionEvent event){
+    private void exitButtonAction(ActionEvent event){
 
         final Node source = (Node) event.getSource();
         final Stage stage = (Stage) source.getScene().getWindow();
         stage.close();
     }
+    
+
+    
+    private void addPartButtonAction(ActionEvent event){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AddPart.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        }catch(Exception e){
+            
+        }
+    }
+    
+
+
+    
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
